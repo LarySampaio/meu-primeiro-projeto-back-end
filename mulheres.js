45a484beb71de3bcf69039ecd7e71968d9077557
LarySampaio <lahryssah2017@gmail.com>
2024-09-20 18:59:04 -0300

    adiciona a primeira versão do meu projeto

---
+++ mulheres.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+
+const app = express();
+const porta = 3333;
+
+const mulheres = [
+  {
+    nome: "Larissa Sampaio",
+    imagem:
+      "https://media.licdn.com/dms/image/v2/D5603AQEBPB7LmjAKHQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1715013586609?e=1732147200&v=beta&t=C75a_fGoEcJxMwFikcbJ3COBwCzApMzdbv907hmaPJQ",
+    minibio: "Desenvolvedora",
+  },
+  {
+    nome: "Larissa Sampaio",
+    imagem:
+      "https://media.licdn.com/dms/image/v2/D5603AQEBPB7LmjAKHQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1715013586609?e=1732147200&v=beta&t=C75a_fGoEcJxMwFikcbJ3COBwCzApMzdbv907hmaPJQ",
+    minibio: "Desenvolvedora",
+  },
+  {
+    nome: "Larissa Sampaio",
+    imagem:
+      "https://media.licdn.com/dms/image/v2/D5603AQEBPB7LmjAKHQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1715013586609?e=1732147200&v=beta&t=C75a_fGoEcJxMwFikcbJ3COBwCzApMzdbv907hmaPJQ",
+    minibio: "Desenvolvedora",
+  },
+];
+
+function mostraMulheres(requeste, response) {
+  response.json(mulheres);
+}
+
+function mostraPorta() {
+  console.log("Servidor criado e rodando na porta ", porta);
+}
+
+app.use(router.get("/mulheres", mostraMulheres));
+app.listen(porta, mostraPorta);
